d586baf8e8165d720d80b4755e20beb6233f084c
Nguyen Tri Tin <tinnguyen5071@gmail.com>
2025-09-17 11:02:54 +0700

    First commit

---
+++ src/test/java/vn/kltn/KLTN/KltnApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.kltn.KLTN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
